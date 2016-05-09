97e005f917b1a19d4fde4fe3f3afcf4a41d4a6c8
Luiz Picolo <luizpicolo@gmail.com>
2016-05-09 16:52:07 -0400

    Added plugin CodeGlance

---
+++ config/rubystubs/ruby18/nokogumbo/1.4.1/nokogumbo.rb
@@ -0,0 +1,11 @@
+=begin
+ This is a machine generated stub using stdlib-doc for <b>class Nokogumbo</b>
+ Sources used:  gem nokogumbo, v. 1.4.1
+ Created on 2016-05-06 08:46:11 -0400 by IntelliJ Ruby Stubs Generator.
+=end
+
+class Nokogumbo
+    def self.parse(p1)
+        #This is a stub, used for indexing
+    end
+end
